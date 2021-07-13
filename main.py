c8ed9779f33ca4461d54e429fcbf012659355aba
FromBrazilZvezda <87343418+FromBrazilZvezda@users.noreply.github.com>
2021-07-13 20:25:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,27 +11,77 @@ auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(acess_key, acess_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-## CONFIGURAÇÕES DE DIA ENPE ##
-days = 137
-total_days = 138
+## CONFIGURAÇÕES FIXAS ##
+post_hour = 00
+post_minute = 00
+tittle = 'days.txt'
 
-# CONFIGURAÇÕES DE HORARIO DE POST ##
-hour = 21
-minute = 30
+## CONFIGURAÇÕES DE FUNÇÕES ##
+def read_vac(tittle):
+    archive = open(tittle, 'r')
+    separation = []
+    for item in archive.readlines():
+        separation.append(item)
+    archive.close()
+    vac = int(separation[0][33:35])
+    return vac
 
-## CONFIGURAÇÕES NÃO ALTERÁVEIS ##
-days_remaining = -(days - total_days)
-perc = ((days - total_days) / total_days) * 100
 
-## CONFIGURAÇÕES DE POST ##
-while True:
-    x = time.gmtime()
-    if x[3] == int(hour) and x[4] == int(minute):
-        post_frase =  f'Faltam {days} dias para o final do ENPE 2021/1 na @UFSCaroficial.'
-        api.update_status(post_frase)
-        print('Atualizado.')
-        days = days - 1
-        time.sleep(60)
-    else:
-        time.sleep(60)
-        print('Não há o que atualizar.')
+def read_days(tittle):
+    archive = open(tittle, 'r')
+    separation = []
+    for item in archive.readlines():
+        separation.append(item)
+    archive.close()
+    days = int(separation[1][13:16])
+    return days
+
+
+def read_total_days(tittle):
+    archive = open(tittle, 'r')
+    separation = []
+    for item in archive.readlines():
+        separation.append(item)
+    archive.close()
+    total_days = int(separation[1][22:25])
+    return total_days
+
+
+def change_days(tittle):
+    archive = open(tittle, 'r')
+    separation = []
+    for item in archive.readlines():
+        separation.append(item)
+    archive.close()
+    archive_2 = open('days.txt', 'w')
+    
+    vac = int(separation[0][33:35])
+    new_vacation_days = 'vaccation_total_days_remaining = {}\n'.format(vac - 1)
+    separation[0] = new_vacation_days
+
+    days = int(separation[1][13:16])
+    new_days = 'total_days = {}\n'.format(days + 1)
+    separation[1] = new_days
+    
+    for item in separation:
+        archive_2.write(item)
+
+    archive_2.close
+
+
+def main():
+    while True:
+        x = time.gmtime()
+        if x[3] == int(post_hour) and x[4] == int(post_minute):
+            post_frase =  f'Ainda temos {read_vac(tittle)} de férias. O ENPE 2021/1 se inicia em 16 de Agosto de 2021. (Tentativa 3)'
+            api.update_status(post_frase)
+            print('Atualizado.')
+            change_days(tittle)
+            time.sleep(60)
+        else:
+            print('Não há o que atualizar.')
+            time.sleep(60)
+            
+
+if __name__ == '__main__':
+    main()
