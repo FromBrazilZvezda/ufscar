d018175931147b65f92de9f26fd8ebd86feda5ed
FromBrazilZvezda <87343418+FromBrazilZvezda@users.noreply.github.com>
2021-07-13 00:06:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,36 @@ from os import environ
 import tweepy
 import time
 
-api_key = 'bFHqN6CHQVroUdXaGIOBlXrM2'
-api_secret_key = 'JBgogY8HLwhzuupEYsCbKA4mTMJaV87PWj0O3AG4daghUg9vi2'
-acess_key = '1404917255100637195-tteBC18NdLSdL0m1h7qHEj8zXiv2Lg'
-acess_secret = 'rqaK5LUhXyuAtzIIuhr4AfjRYI21qBGPXUy8S4MAnViat'
+## CONFIGURAÇÕES DO TWITTER ## 
+api_key = environ['api_key']
+api_secret_key = environ['api_secret_key']
+acess_key = environ['acess_key']
+acess_secret = environ['acess_secret']
 auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(acess_key, acess_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
+## CONFIGURAÇÕES DE DIA ENPE ##
 days = 138 
 total_days = 138
+
+# CONFIGURAÇÕES DE HORARIO DE POST ##
+hour = 3
+minute = 10
+
+## CONFIGURAÇÕES NÃO ALTERÁVEIS ##
 days_remaining = -(days - total_days)
 perc = ((days - total_days) / total_days) * 100
 
+## CONFIGURAÇÕES DE POST ##
 while True:
     x = time.gmtime()
-    if x[3] == 21 and x[4] == 30:
-        post_frase =  f'Faltam {days} dias para o final do ENPE 2021/2 na @UFSCaroficial.'
+    if x[3] == int(hour) and x[4] == int(minute):
+        post_frase =  f'Faltam {days} dias para o final do ENPE 2021/1 na @UFSCar.'
         api.update_status(post_frase)
-        print('postado!')
+        print('Atualizado.')
         days = days - 1
         time.sleep(60)
     else:
         time.sleep(60)
+        print('Não há o que atualizar.')
