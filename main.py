670ec55b32f57ed131a30d4cceb471c62e72df53
FromBrazilZvezda <87343418+FromBrazilZvezda@users.noreply.github.com>
2021-07-15 16:19:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from os import environ
 import tweepy
-import time
+from os import environ
+from time import sleep, gmtime
 
-## CONFIGURAÇÕES DO TWITTER ## 
+# Time_config #
+post_h = 21
+post_m = 0
+
+# Tweepy_config #
 api_key = environ['api_key']
 api_secret_key = environ['api_secret_key']
 acess_key = environ['acess_key']
@@ -11,77 +15,49 @@ auth = tweepy.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(acess_key, acess_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-## CONFIGURAÇÕES FIXAS ##
-post_hour = 23
-post_minute = 37
-tittle = 'days.txt'
+# days_config #
+vacation_days = 32
+study_days = -32
+total_study_days = 103
 
-## CONFIGURAÇÕES DE FUNÇÕES ##
-def read_vac(tittle):
-    archive = open(tittle, 'r')
-    separation = []
-    for item in archive.readlines():
-        separation.append(item)
-    archive.close()
-    vac = int(separation[0][33:35])
-    return vac
+# math_config #
+percentage = (study_days / total_study_days) * 100
 
-
-def read_days(tittle):
-    archive = open(tittle, 'r')
-    separation = []
-    for item in archive.readlines():
-        separation.append(item)
-    archive.close()
-    days = int(separation[1][13:16])
-    return days
-
-
-def read_total_days(tittle):
-    archive = open(tittle, 'r')
-    separation = []
-    for item in archive.readlines():
-        separation.append(item)
-    archive.close()
-    total_days = int(separation[1][22:25])
-    return total_days
-
-
-def change_days(tittle):
-    archive = open(tittle, 'r')
-    separation = []
-    for item in archive.readlines():
-        separation.append(item)
-    archive.close()
-    archive_2 = open('days.txt', 'w')
-    
-    vac = int(separation[0][33:35])
-    new_vacation_days = 'vaccation_total_days_remaining = {}\n'.format(vac - 1)
-    separation[0] = new_vacation_days
-
-    days = int(separation[1][13:16])
-    new_days = 'total_days = {}\n'.format(days + 1)
-    separation[1] = new_days
-    
-    for item in separation:
-        archive_2.write(item)
-
-    archive_2.close
-
-
-def main():
-    while True:
-        x = time.gmtime()
-        if x[3] == int(post_hour) and x[4] == int(post_minute):
-            post_frase =  f'Ainda temos {read_vac(tittle)} de férias. O ENPE 2021/1 se inicia em 16 de Agosto de 2021.'
-            api.update_status(post_frase)
+# post_config #
+while True:
+    actual_time = gmtime()
+    if actual_time[3] == int(post_h) and actual_time[4] == int(post_m):
+        if vacation_days > 0:
+            post_frase = f'Ainda temos {vacation_days} de férias. O ENPE 2021/1 se inicia em 16 de Agosto de 2021.'
+            vacation_days = vacation_days - 1
+            study_days = study_days + 1
             print('Atualizado.')
-            change_days(tittle)
-            time.sleep(60)
-        else:
-            print('Não há o que atualizar.')
-            time.sleep(60)
+            sleep(60)
             
 
-if __name__ == '__main__':
-    main()
+        elif vacation_days == 0 and study_days == 0:
+            post_frase = f'As aulas começam amanhã! Boa sorte a todos <3'
+            study_days = study_days + 1
+            api.update_status(post_frase)
+            print('Atualizado.')
+            sleep(60)
+            
+        elif 0 < study_days > 103:
+            post_frase = 'Já se passaram {} dias de aula no ENPE 2021/1. Completamos um total de {:.2f}% do semestre!'.format(study_days, percentage)
+            study_days = study_days + 1
+            api.update_status(post_frase)
+            print('Atualizado.')
+            sleep(60)
+            
+        elif study_days == total_study_days:
+            break
+    else:
+        print('Nada para atualizar.')
+        sleep(60)
+        
+# end_season_config #
+post_frase = '"Nós é merecedor de curti umas férias", RODO, Poze do. 2020.\nA partir de agora nós entraremos em stand-by para manutenção. Foi um prazer dividir esse semestre com vocês!'
+api.update_status(post_frase)
+print('Atualizado.')
+
+ 
